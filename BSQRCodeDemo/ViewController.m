2ecb47f5c7e69496c07940baa9dbe838df465de5
1vanFan <fbsdtc@163.com>
2017-05-31 18:42:40 +0800

    test

--- BSQRCodeDemo/ViewController.m
+++ BSQRCodeDemo/ViewController.m
@@ -23,6 +23,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(refreshLabel:) name:BSRefreshNotifycation object:nil];
 }
 
